bf162099ee45e3bba1dacc617353764177dbf6cf
Egor Quenum <georgequenum@yahoo.com>
2024-05-14 00:37:24 +0400

    Configure load shedding

--- LoadShedding/Program.cs
+++ LoadShedding/Program.cs
@@ -1,3 +1,7 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace LoadShedding.Api;
 
@@ -7,16 +11,29 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddLoadShedding((options) => 
+        {
+            // https://farfetch.github.io/loadshedding/docs/guides/adaptative-concurreny-limiter/configuration
+            // options.AdaptativeLimiter.ConcurrencyOptions.MinQueueSize = 10;
+            // options.AdaptativeLimiter.ConcurrencyOptions.MinConcurrencyLimit = 5;
+            // options.AdaptativeLimiter.ConcurrencyOptions.InitialConcurrencyLimit = 5;
+            // options.AdaptativeLimiter.ConcurrencyOptions.InitialQueueSize = 50;
+            // options.AdaptativeLimiter.ConcurrencyOptions.Tolerance = 2;
+            // options.AdaptativeLimiter.ConcurrencyOptions.QueueTimeoutInMs = 60000;
+
+            options.AdaptativeLimiter.UseEndpointPriorityResolver();
+            options.SubscribeEvents(events => 
+            {
+                // other available are ItemEnqueued, ItemDequeued, ItemProcessing and ItemProcessed
+                events.Rejected.Subscribe(args => Console.Error.WriteLine($"Item rejected with Priority: {args.Priority}"));
+            });
+        });
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -27,7 +44,7 @@ public class Program
 
         app.UseAuthorization();
 
-
+        app.UseLoadShedding();
         app.MapControllers();
 
         app.Run();
