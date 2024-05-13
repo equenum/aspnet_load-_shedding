c6479a4cc61b69a7be51921d3d5c21e3904911f0
Egor Quenum <georgequenum@yahoo.com>
2024-05-14 00:36:55 +0400

    Implement controllers

---
+++ LoadShedding/Controllers/MediaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Farfetch.LoadShedding.AspNetCore.Attributes;
+using Farfetch.LoadShedding.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoadShedding.Api;
+
+[ApiController]
+[Route("api/public/[controller]")]
+public class MediaController : ControllerBase
+{
+    [HttpGet("stream")]
+    [EndpointPriority(Priority.Critical)]
+    public IActionResult GetMediaStream()
+    {
+        var bytes = new byte[100000];
+        
+        // generate random bytes to imitate a workload
+        var random = new Random();
+        random.NextBytes(bytes);
+
+        return Ok(string.Join(" ", bytes));
+    }
+}
---
+++ LoadShedding/Controllers/UserPreferences.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Farfetch.LoadShedding.AspNetCore.Attributes;
+using Farfetch.LoadShedding.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoadShedding.Api;
+
+[ApiController]
+[Route("api/public/[controller]")]
+public class UserPreferencesController : ControllerBase
+{
+    [HttpGet("lang")]
+    [EndpointPriority(Priority.NonCritical)]
+    public async Task<IActionResult> GetLanguageAsync()
+    {
+        await Task.Delay(1000);
+        return Ok("English");
+    }
+}
